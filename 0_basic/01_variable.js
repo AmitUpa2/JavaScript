59b13c990ebc123d8da092dbde7a2efdfc30b773
Amit kumar <147317695+AmitUpa2@users.noreply.github.com>
2024-12-17 05:46:08 +0000

    variables

---
+++ 0_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12334;
+let accountEmail = "amit@12gmail.com";
+var accountPassword = "xnjd23";
+accountCity = "noida";
+let accountState;
+// accountId = 34;  // this is not allowed 
+console.log(accountId);
+
+accountEmail = "amit@46778.gmail.com";
+accountPassword = "124346";
+accountCity = "delhi"
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
